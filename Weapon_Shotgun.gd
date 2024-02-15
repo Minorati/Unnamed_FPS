cc23279175a296553613e6873cbb27ae8fb75cd6
donwung <113479299+donwung@users.noreply.github.com>
2024-02-14 22:36:18 -0800

    added clicking for weapon

--- Weapon_Shotgun.gd
+++ Weapon_Shotgun.gd
@@ -8,7 +8,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-func _unhandled_key_input(event):
-	pass
+	if Input.is_action_just_pressed("mb1"):
+		print("click")
